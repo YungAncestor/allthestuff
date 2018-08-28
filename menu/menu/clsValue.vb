1ca4bed3f8d90c777119c3433c2287d84c6da2ae
itzwallker <itzwallker@users.noreply.github.com>
2018-08-27 23:28:06 -0300

    new menu

---
+++ menu/menu/clsValue.vb
@@ -0,0 +1,12 @@
+﻿'clsValues.cls
+'credits to @itzwallker
+
+
+Public Class clsValue
+    Public selected2 As Boolean
+    Public selected3 As Integer
+    Public selec1 As Boolean
+    Public selec2 As Boolean
+    Public selec3 As Boolean
+
+End Class
